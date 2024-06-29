4b3c6b44a367c31587d6560d79816d676e70d86e
est3banst <estebeeen@outlook.com>
2024-06-29 18:15:41 -0300

    eh

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,32 @@ const app = express();
 const router = express.Router();
 const posts = {};
 
+const authUser = (req, res, next) => {
+    const user = req.body.user;
+    const password = req.body.password
+    if (!user) {
+
+    }
+    if (!password) {
+
+    }
+
+}
+
+router.get('/', (req, res) => {
+    const metadata = {
+        title : 'My Dev Tour',
+        description: 'Blog about technology and programming languages',
+    }
+    res.render('mainboard', { metadata })
+})
+router.get('/post/:id', (req, res) => {
+    const metadata = {
+        title : 'My Dev Tour',
+        description: 'Blog about technology and programming languages',
+    }
+    res.render('/', { metadata })
+})
 
 router.post('/add_post',(req, res) => {
     const title = req.body.title;
@@ -16,10 +42,10 @@ router.post('/add_post',(req, res) => {
 
 })
 
-router.put('/edit_post',(req, res) => {
+router.put('/edit_post/:id',(req, res) => {
     
 })
-router.delete('/delete_post',(req, res ) => {
+router.delete('/delete_post/:id',(req, res ) => {
 
 })
 
